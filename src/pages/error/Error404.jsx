c7dec31284181388020c76b343cc4dfd4f91148a
om praksash pattjoshi <omprakashpattjoshi123@gmail.com>
2022-05-04 22:28:35 +0530

    add style in 404 error page

--- src/pages/error/Error404.jsx
+++ src/pages/error/Error404.jsx
@@ -5,7 +5,22 @@ import styled from "styled-components";
 import errimg from "../../Image/404.png";
 
 const Image404 = styled.img`
-  height: 100vh;
+  height: 50%;
+  width: 50%;
+  display: flex;
+  text-align: center;
+  display: block;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  margin-bottom: 25px;
+  @media screen and (max-width: 800px) {
+    height: 70%;
+    width: 70%;
+    padding: 10px;
+    align-items: center;
+    margin-bottom: 20px;
+  }
 `;
 
 const Error404 = () => {
